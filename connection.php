a9a0f1fcbb37969398c08c5e54203fde798eac38
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-21 11:59:48 +0200

    update

--- connection.php
+++ connection.php
@@ -1,6 +1,22 @@
 <?php
 // Database connection using environment variables
-$dsn = getenv('DATABASE_URL');
+$databaseUrl = getenv('DATABASE_URL');
+
+if ($databaseUrl === false) {
+    echo "Environment variable DATABASE_URL not set.";
+    exit;
+}
+
+$db = parse_url($databaseUrl);
+
+$dsn = sprintf(
+    'pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s',
+    $db['host'],
+    $db['port'],
+    ltrim($db['path'], '/'),
+    $db['user'],
+    $db['pass']
+);
 
 try {
     $pdo = new PDO($dsn, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
---
+++ debug.php
@@ -0,0 +1,5 @@
+<?php
+// Debug: Print all environment variables
+print_r($_ENV);
+print_r(getenv());
+?>
