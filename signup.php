126462232a4bc94df807f261d1b86bee0a05dc11
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-21 11:02:01 +0200

    updates

--- signup.php
+++ signup.php
@@ -1,14 +1,6 @@
 <?php
-// Database connection
-$dsn = 'pgsql:host=c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com;port=5432;dbname=dd1nrhh4asd5js;user=ub503r7djq1bfj;password=p35df812fa54b039bac55e4ffc411820ba0f4a12f70009a2efc52857f26072ba4';
-
-try {
-    $pdo = new PDO($dsn, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo "Connection successful!<br>";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit;
-}
+// Include the database connection
+include 'connection.php';
 
 // Example of a signup form handling
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
