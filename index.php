39b79dbc7ae8da32b5cb174d488a5f81511912d3
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-22 11:48:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,9 @@
     <header class="navbar-section">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><i class="bi bi-chat"></i>GETFLIX</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                    
+                    <a class="navbar-brand" href="#"><i class="bi bi-chat"></i>GETFLIX</a>
+                
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="#home">Home</a>
@@ -33,7 +30,6 @@
                             <a class="nav-link" href="signup.php">signup</a>
                         </li>
                     </ul>
-                </div>
             </div>
         </nav>
     </header>
