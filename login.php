2181c1d95fbdc612d4ceacc09815bca126cfee71
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-22 14:23:23 +0200

    update

--- login.php
+++ login.php
@@ -84,6 +84,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
      <!-- hero section  -->
      <section id="home" class="hero-section">
+        <div class="container" id="LoginForm">
+            
                         <form method="post" action="login.php">
                         <label for="username">Username:</label>
                         <input type="text" id="username" name="username" required>
@@ -93,6 +95,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <br>
                         <button type="submit">Login</button>
                         </form>
+           
+        </div>
     </section>
 
     <!-- footer section  -->
