9377b6cde22db8b8f736c9a7a49f9cb21db0a799
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-22 11:40:09 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,4 @@
 <?php
-require 'vendor/autoload.php'; // Include Composer's autoloader
-
 use Dotenv\Dotenv;
 
 // Load .env file
@@ -15,8 +13,10 @@ if ($databaseUrl === false) {
     exit;
 }
 
+//Parse the DATABASE_URL into its components (host, port, dbname, user, password).
 $db = parse_url($databaseUrl);
 
+// Create the DSN (Data Source Name) string for the PDO connection
 $dsn = sprintf(
     'pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s',
     $db['host'],
@@ -26,4 +26,15 @@ $dsn = sprintf(
     $db['pass']
 );
 
+// Establish the PDO connection
+try {
+    // PDO (PHP Data Objects) is a database access layer providing a uniform method of access to multiple databases.
+    // Here, we create a new PDO instance using the DSN and set error mode to exception.
+    $pdo = new PDO($dsn, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    // If the connection fails, an exception is thrown and caught here.
+    // The error message is displayed, and the script exits.
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
 ?>
