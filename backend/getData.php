e328d2695da0b9d5d539ab4d91c9eb3c01c4a49d
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-08-22 09:27:23 +0200

    Update getData.php

--- backend/getData.php
+++ backend/getData.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include '../backend/API_KEY.php';
+include __DIR__ . '/API_KEY.php';
 
 try {
     // Get query parameters
